60fe26a8a6420ac793f9ad22c679521937002cc8
Alex Beciana <alex.beciana@gmail.com>
2020-06-16 04:25:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(path)
   emoticons.each_with_object({}) do |(key, value), final_array|
     final_array[key] = {}
     final_array[:english] = value[0]
-    final_array[:english] = value[1]
+    final_array[:japanese] = value[1]
   end
 end
 
