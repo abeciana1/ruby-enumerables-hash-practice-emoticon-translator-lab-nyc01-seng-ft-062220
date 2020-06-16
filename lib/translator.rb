5393b1b91627c280180adbf92bac006fe75f0106
Alex Beciana <alex.beciana@gmail.com>
2020-06-16 04:25:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,6 @@ def load_library(path)
   emoticons = YAML.load_file(path)
   
   emoticons.each_with_object({}) do |(key, value), final_array|
-    binding.pry
     final_array[key] = {}
     final_array[:english] = value[0]
     final_array[:english] = value[1]
