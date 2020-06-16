789ba54cf152e2a6cadd0102ccb0f59a5fd64708
Alex Beciana <alex.beciana@gmail.com>
2020-06-16 04:19:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,9 @@ require 'pry'
 def load_library(path)
   emoticons = YAML.load_file(path)
   
-  
+  emoticons.each_with_object do |(key, value)|
+    binding.pry
+  end
 end
 
 def get_japanese_emoticon(path, emoticon1)
